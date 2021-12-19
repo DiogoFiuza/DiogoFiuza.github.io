84c92e1674f6e0963c08f39edac69221176e202d
DiogoFiuza <difipalm@gmail.com>
2021-12-19 19:18:01 -0300

    stacks adicionados com o estilo

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Aboutme from "../components/Aboutme";
 import Apresentation from "../components/Apresentation";
 import Header from "../components/Header";
+import Stacks from "../components/Stacks";
 
 function MainPage(){
     return(
@@ -9,6 +10,7 @@ function MainPage(){
         <Header />
         <Apresentation />
         <Aboutme />
+        <Stacks />
       </>
     );
 }
