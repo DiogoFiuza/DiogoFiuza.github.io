c40459fd217d3abeb1d70fce9abadae00de13dbc
DiogoFiuza <difipalm@gmail.com>
2021-12-21 18:39:49 -0300

    styled scrollbar

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,6 +3,7 @@ import Aboutme from "../components/Aboutme";
 import Apresentation from "../components/Apresentation";
 import Header from "../components/Header";
 import Stacks from "../components/Stacks";
+import "../styles/MainPage.css"
 
 function MainPage(){
     return(
