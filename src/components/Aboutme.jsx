26da82c8b5026d190dd49b8d4e992a4941986216
DiogoFiuza <difipalm@gmail.com>
2022-04-15 12:59:53 -0300

    Feat: Eslint adicionado ao projeto

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,28 +1,40 @@
-import React from "react";
-import '../styles/Aboutme.css'
-import profile_pic from '../imagens/profile-pic.png'
-import download_icon from '../imagens/download_icon.png'
+import React from 'react';
+import '../styles/Aboutme.css';
 import AOS from 'aos';
+import profilePic from '../imagens/profile-pic.png';
+import downloadIcon from '../imagens/download_icon.png';
 import 'aos/dist/aos.css';
 
 AOS.init();
 
-function Aboutme(){
+function Aboutme() {
   return (
     <>
-        <h1 className="aboutMe_title">About Me</h1>
-        <hr className='line_below_title' />
-      <div className='aboutMe_container'>
-          <div data-aos="fade-up" className='aboutMe'>
-            <img src={ profile_pic } alt="my_picture" />
-            <span className="name">Diogo Fiuza Palmeira</span>
-            <span className="title">Web Developer Full Stack Júnior</span>
-          </div>
-          <hr className='line_divides_text'/>
-          <p className="aboutme_text">Sou um Dev de 24 anos natural de Brasília, em busca de desafios que possam me desenvolver. Atualmente sou formado em Administração, faço graduação em Análise e Desenvolvimento de Sistemas, curso de desenvovimento Full Stack na <a href='https://www.betrybe.com/' alt='site_da_trybe' target="_blank" rel="noreferrer">Trybe</a> e inglês na Wise up. Gosto de aprender e de criar conexões, nas horas vagas gosto de bater uma bolinha e jogar uns games. 
+      <h1 className="aboutMe_title">About Me</h1>
+      <hr className="line_below_title" />
+      <div className="aboutMe_container">
+        <div data-aos="fade-up" className="aboutMe">
+          <img src={profilePic} alt="my_picture" />
+          <span className="name">Diogo Fiuza Palmeira</span>
+          <span className="title">Web Developer Full Stack Júnior</span>
+        </div>
+        <hr className="line_divides_text" />
+        <p className="aboutme_text">
+          Sou um Dev de 24 anos natural de Brasília,
+          em busca de desafios que possam me desenvolver.
+          Atualmente sou formado em Administração,
+          faço graduação em Análise e Desenvolvimento de Sistemas,
+          curso de desenvovimento Full Stack na Trybe
+          e inglês na Wise up. Gosto de aprender e de criar conexões,
+          nas horas vagas gosto de bater uma bolinha e jogar uns games.
           <br />
-          <button className='curriculo_button'>Currículo <img className='download_icon' src={download_icon} alt="download" /> </button>
-          </p>
+          <a href="Currículo - Diogo.pdf" download="Currículo - Diogo">
+            <button type="button" className="curriculo_button">
+              Currículo
+              <img className="download_icon" src={downloadIcon} alt="download" />
+            </button>
+          </a>
+        </p>
       </div>
     </>
   );
--- src/components/Apresentation.jsx
+++ src/components/Apresentation.jsx
@@ -4,13 +4,21 @@ import avatar from '../imagens/avatar.png'
 
 function Apresentation(){
   return (
-    <>
-      <h1 className="hello_word">HELLO WORLD.</h1>
-      <div className="retangle_1" />
-      <div className="retangle_2" />
-      <img className="avatar" src={ avatar } alt="profile"/>
-      <p className="i_am">I’m Diogo Fiuza, Web Developer</p>
-    </>
+    <div className="apresentation">
+      <div className="apresentation-container">
+        <h1 className="hello_word">
+          HELLO
+          <br />
+          WORLD.
+          </h1>
+        <p className="i_am">I’m Diogo Fiuza, Web Developer</p>
+      </div>
+      <div className="main-retangle">        
+        <div className="retangle_1" />
+        <div className="retangle_2" />
+        <img className="avatar" src={ avatar } alt="profile"/>
+      </div>
+    </div>
   );
 }
 
