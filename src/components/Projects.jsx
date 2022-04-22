33f9344628b5a1b971e57109a727a6f8e0fa80fb
DiogoFiuza <difipalm@gmail.com>
2022-04-22 17:04:32 -0300

    Feat: Criando o componente projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import myProjects from '../utils/myProjects';
+import '../styles/Projects.css';
+
+function Projects() {
+  return (
+    <div>
+      <h1 className="aboutMe_title" id="Projects">Projects</h1>
+      <hr className="line_below_title" />
+      <div className="carousel">
+        {myProjects.map((proj) => (
+          <div className="cardOfProject">
+            <h4 className="prokName">{proj.name}</h4>
+            <img className="projImage" src={proj.image} alt="imagem" />
+            <div className="buttonLink">
+              <a target="blank" href={proj.repo}>GitHub</a>
+              <a target="blank" href={proj.link}>Link</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
---
+++ src/utils/myProjects.js
@@ -0,0 +1,30 @@
+import image from '../imagens/projectML.png';
+
+const myProjects = [
+  {
+    name: 'Shopping Cart',
+    link: 'https://shopping-cart-3wd5e9d17-diogofiuza.vercel.app/',
+    repo: 'https://github.com/DiogoFiuza/Shopping-Cart',
+    image,
+  },
+  {
+    name: 'Shopping Cart',
+    link: 'https://shopping-cart-3wd5e9d17-diogofiuza.vercel.app/',
+    repo: 'https://github.com/DiogoFiuza/Shopping-Cart',
+    image,
+  },
+  {
+    name: 'Shopping Cart',
+    link: 'https://shopping-cart-3wd5e9d17-diogofiuza.vercel.app/',
+    repo: 'https://github.com/DiogoFiuza/Shopping-Cart',
+    image,
+  },
+  {
+    name: 'Shopping Cart',
+    link: 'https://shopping-cart-3wd5e9d17-diogofiuza.vercel.app/',
+    repo: 'https://github.com/DiogoFiuza/Shopping-Cart',
+    image,
+  },
+];
+
+export default myProjects;
